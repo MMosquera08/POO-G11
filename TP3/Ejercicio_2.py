057ba067bce331fcf9fc1b20a133f2446cd75a50
EGhilardi <ghilardiemilia-sec@instituto-sanpablo.com>
2025-04-10 13:17:43 -0300

    TP3 - Ejercicios

--- TP3/Ejercicio_2.py
+++ TP3/Ejercicio_2.py
@@ -1,1 +1,5 @@
 print("Ejercicio 2 - Contador de Letras")
+palabra = input("Ingrese una palabra: ")
+for i in palabra:
+
+
--- TP3/Ejercicio_3.py
+++ TP3/Ejercicio_3.py
@@ -1,14 +1,15 @@
 print("Ejercicio 3 - Adivina el Número")
 numsecreto = 86
-i=0
+i = 0
 num = int(input("Intenta adivinar el número secreto: "))
 while i <= 8:
     num = int(input("Intenta adivinar de nuevo:"))
-    i=i+1
+    i = i + 1
     if num < numsecreto:
         print("El número ingresado es menor al correcto.")
     elif num > numsecreto:
         print("El número ingresado es mayor al correcto.")
     elif num == numsecreto:
         print("Ganaste.")
+        break
 print("Perdiste.")
