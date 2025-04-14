bcf4b9f7a42c5bb99e95d5acb30922f0a65392d3
EGhilardi <ghilardiemilia-sec@instituto-sanpablo.com>
2025-04-14 11:14:20 -0300

    Ejercicio_2.py

--- TP3/Ejercicio_2.py
+++ TP3/Ejercicio_2.py
@@ -1,5 +1,15 @@
 print("Ejercicio 2 - Contador de Letras")
-palabra = input("Ingrese una palabra: ")
-for i in palabra:
-
-
+a = 0
+v = 0
+c = 0
+while a == " ":
+    a = input("Ingrese una palabra o frase: ")
+    for i in a:
+        if i == "a" or i == "e" or i == "i" or i == "o" or i == "u":
+            v += 1
+        else:
+            c += 1
+        if v > 5:
+            a = input("Ingrese otra palabra o frase: ")
+    print(v, "es la cantidad de vocales.")
+    print(c, "es la cantidad de consonantes.")
