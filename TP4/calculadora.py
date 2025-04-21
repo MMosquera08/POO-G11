c6b2bd1ee6c152f2a28d2e8c4260b166f41ca892
EGhilardi <ghilardiemilia-sec@instituto-sanpablo.com>
2025-04-21 10:24:37 -0300

    calculadora.py

--- TP4/calculadora.py
+++ TP4/calculadora.py
@@ -7,25 +7,15 @@ while repetir > 0:
     print("3 = Multiplicación")
     print("4 = División")
     operacion = int(input("Elija una operación: "))
+    num1 = int(input("Ingrese el primer número: "))
+    num2 = int(input("Ingrese el segundo número: "))
     if operacion == 1:
-        print("Suma")
-        num1 = int(input("Ingrese el primer número: "))
-        num2 = int(input("Ingrese el segundo número: "))
         print("La suma de los dos números es: ", num1 + num2)
     if operacion == 2:
-        print("Resta")
-        num1 = int(input("Ingrese el primer número: "))
-        num2 = int(input("Ingrese el segundo número: "))
         print("La resta de los dos números es: ", num1 - num2)
     if operacion == 3:
-        print("Multiplicación")
-        num1 = int(input("Ingrese el primer número: "))
-        num2 = int(input("Ingrese el segundo número: "))
         print("La multiplicación de los dos números es: ", num1 * num2)
     if operacion == 4:
-        print("División")
-        num1 = int(input("Ingrese el primer número: "))
-        num2 = int(input("Ingrese el segundo número: "))
         if num2 != 0:
             print("La división de los dos números es: ", num1 / num2)
         else:
