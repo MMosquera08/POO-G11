442b35e8369fb5d27a91f38aef8dbece32f74d12
EGhilardi <ghilardiemilia-sec@instituto-sanpablo.com>
2025-05-15 10:15:31 -0300

    calculadora - grupo 11

--- TP5/calculadora2.py
+++ TP5/calculadora2.py
@@ -2,30 +2,58 @@ import funciones as f
 
 print("Calculadora")
 
+ans=0
 while True:
-    print("1 - Suma")
-    print("2 - Resta")
-    print("3 - Multiplicación")
-    print("4 - División")
-    print("5 - Salir")
-    print("6 - Utilizar resultado anterior")
-    operacion = int(input("Seleccione una operación: "))
-    if operacion == 1:
-        x1, x2 = f.ingresar_datos()
-        R = f.suma(x1, x2)
-        print("El resultado es ", R)
-    elif operacion == 2:
-        x1, x2 = f.ingresar_datos()
-        R = f.resta(x1, x2)
-        print("El resultado es ", R)
-    elif operacion == 3:
-        x1, x2 = f.ingresar_datos()
-        R = f.multiplicacion(x1, x2)
-        print("El resultado es ", R)
-    elif operacion == 4:
-        x1, x2 = f.ingresar_datos()
-        R = f.division(x1, x2)
-        print("El resultado es ", R)
-    elif operacion == 5:
-        break
-    elif operacion == 6:
+    uso_ans = input("¿Quiere usar ans, si o no?: ")
+    if uso_ans == "no":
+        print("1 - Suma")
+        print("2 - Resta")
+        print("3 - Multiplicación")
+        print("4 - División")
+        print("5 - Salir")
+        operacion = (input("Seleccione una operación: "))
+        if operacion == "1":
+            x1, x2 = f.ingresar_datos()
+            R = f.suma(x1, x2)
+            print("El resultado es ", R)
+        elif operacion == "2":
+            x1, x2 = f.ingresar_datos()
+            R = f.resta(x1, x2)
+            print("El resultado es ", R)
+        elif operacion == "3":
+            x1, x2 = f.ingresar_datos()
+            R = f.multiplicacion(x1, x2)
+            print("El resultado es ", R)
+        elif operacion == "4":
+            x1, x2 = f.ingresar_datos()
+            R = f.division(x1, x2)
+            print("El resultado es ", R)
+        elif operacion == "5":
+            break
+        ans=R
+    elif uso_ans == "si":
+        print("1 - Suma")
+        print("2 - Resta")
+        print("3 - Multiplicación")
+        print("4 - División")
+        print("5 - Salir")
+        operacion = (input("Seleccione una operación: "))
+        if operacion == "1":
+            x1 = f.datos_ans()
+            R = f.suma(x1, ans)
+            print("El resultado es ", R)
+        elif operacion == "2":
+            x1 = f.datos_ans()
+            R = f.resta(x1, ans)
+            print("El resultado es ", R)
+        elif operacion == "3":
+            x1 = f.datos_ans()
+            R = f.multiplicacion(x1, ans)
+            print("El resultado es ", R)
+        elif operacion == "4":
+            x1 = f.datos_ans()
+            R = f.division(x1, ans)
+            print("El resultado es ", R)
+        elif operacion == "5":
+            break
+        ans = R
--- TP5/funciones.py
+++ TP5/funciones.py
@@ -17,3 +17,21 @@ def division(x1, x2):
         return resultado
     else:
         return "No se puede dividir por cero."
+def datos_ans():
+    x1 = int(input("ingrese el primer numero: "))
+    return x1
+def suma(x1, ans):
+    resultado = x1 + ans
+    return resultado
+def resta(x1, ans):
+    resultado = x1 - ans
+    return resultado
+def multiplicacion(x1, ans):
+    resultado = x1 * ans
+    return resultado
+def division(x1, ans):
+    if ans != 0:
+        resultado = x1 / ans
+        return resultado
+    else:
+        return "No se puede dividir por cero."
