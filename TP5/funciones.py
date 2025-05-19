5fb7ecfe6d7c9b8ca75f3afa907d45ac358fefc7
BPera1111 <bruno.peralta@ms.uncu.edu.ar>
2025-05-19 10:08:12 -0300

    "Automated commit grafico"

--- TP5/funciones.py
+++ TP5/funciones.py
@@ -20,18 +20,18 @@ def division(x1, x2):
 def datos_ans():
     x1 = int(input("ingrese el primer numero: "))
     return x1
-def suma(x1, ans):
-    resultado = x1 + ans
-    return resultado
-def resta(x1, ans):
-    resultado = x1 - ans
-    return resultado
-def multiplicacion(x1, ans):
-    resultado = x1 * ans
-    return resultado
-def division(x1, ans):
-    if ans != 0:
-        resultado = x1 / ans
-        return resultado
-    else:
-        return "No se puede dividir por cero."
+# def suma(x1, ans):
+#     resultado = x1 + ans
+#     return resultado
+# def resta(x1, ans):
+#     resultado = x1 - ans
+#     return resultado
+# def multiplicacion(x1, ans):
+#     resultado = x1 * ans
+#     return resultado
+# def division(x1, ans):
+#     if ans != 0:
+#         resultado = x1 / ans
+#         return resultado
+#     else:
+#         return "No se puede dividir por cero."
