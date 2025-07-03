99918280c306ca689494a60efb2f3ae7847e4f75
EGhilardi <ghilardiemilia-sec@instituto-sanpablo.com>
2025-07-03 12:34:52 -0300

    proyecto mascota

--- TP9/Proyecto_mascota.py
+++ TP9/Proyecto_mascota.py
@@ -17,6 +17,7 @@ class Mascota:
         if self.hambre < 0:
             self.hambre = 0
         print(f"¡{self.nombre} corretea feliz! Su felicidad subió.")
+        print(f"felicidad: {self.felicidad}, energía: {self.energia}, hambre: {self.hambre}.")
 
     def comer(self, incremento, decremento):
         self.hambre -= decremento
@@ -26,6 +27,7 @@ class Mascota:
         if self.energia > 100:
             self.energia = 100
         print(f"¡{self.nombre} devoró su comida con ganas! Ya no tiene tanta hambre.")
+        print(f"felicidad: {self.felicidad}, energía: {self.energia}, hambre: {self.hambre}.")
 
     def dormir(self, incremento, decremento):
         self.energia += incremento
@@ -38,6 +40,7 @@ class Mascota:
         if self.hambre < 0:
             self.hambre = 0
         print(f"¡{self.nombre} está echando una siesta! Se ve más enérgico.")
+        print(f"felicidad: {self.felicidad}, energía: {self.energia}, hambre: {self.hambre}.")
 
     def mostrar_estado(self):
         print(f"nombre: {self.nombre}, especie: {self.especie}, felicidad: {self.felicidad}, energía: {self.energia}, hambre: {self.hambre}.")
@@ -60,10 +63,10 @@ while True:
     if accion == "1":
         mascota1.mostrar_estado()
     elif accion == "2":
-        mascota1.jugar(15)
+        mascota1.jugar(15,10)
     elif accion == "3":
-        mascota1.comer(15)
+        mascota1.comer(20,15)
     elif accion == "4":
-        mascota1.dormir(15)
+        mascota1.dormir(15,5)
     elif accion == "5":
         break
